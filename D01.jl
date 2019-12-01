fb0ef70b0a77cdeb84e76bf4eec5c3f2cb4f27b2
csLinhart <36882208+csLinhart@users.noreply.github.com>
2019-12-01 14:06:45 +0100

    add day 1

--- D01.jl
+++ D01.jl
@@ -1,14 +1,12 @@
 using DelimitedFiles
 input = readdlm("D01.txt", '\n', Int)
 
-function CalculateFuel(mass)
-    floor(mass/3) - 2
-end
+CalculateFuel(mass) = floor(mass/3) - 2
 
 println(mapreduce(CalculateFuel, +, input; init=0))
 
 function D1P2(mass)
     fuel = CalculateFuel(mass)
-    return (fuel <= 0) ? 0 : fuel + D1P2(fuel)
+    (fuel <= 0) ? 0 : fuel + D1P2(fuel)
 end
 println(mapreduce(D1P2, +, input; init=0))
