9417f845c89df568e57d1ca40ee6b0894acf07df
Christoph Linhart <christoph.lc.linhart@bmwiti.onmicrosoft.com>
2019-12-05 14:22:13 +0100

    add day 5

---
+++ D05.jl
@@ -0,0 +1,43 @@
+using DelimitedFiles
+inputData = readdlm("D05.txt", ',', Int)
+
+function RunIntcode(input, initInp)
+    global i = 1
+    while true
+        code = parse.(Int, split(lpad(string(input[i]), 4, '0'), ""))
+        opcode = code[end]
+        p1 = code[2] == 1 ? input[i + 1] : input[input[i + 1] + 1]
+        if (input[i + 2] + 1 <= length(input))
+            p2 = code[1] == 1 ? input[i + 2] : input[input[i + 2] + 1]
+        end
+        if opcode == 1
+            input[input[i + 3] + 1] = p1 + p2
+            global i = i + 4
+        elseif opcode == 2
+            input[input[i + 3] + 1] = p1 * p2
+            global i = i + 4
+        elseif opcode == 3
+            input[input[i + 1] + 1] = initInp
+            global i = i + 2
+        elseif opcode == 4
+            if (p1 != 0)
+                println(p1)
+                return
+            end
+            global i = i + 2
+        elseif opcode == 5
+            i = (p1 != 0 ) ? p2 + 1 : i + 3
+        elseif opcode == 6
+            i = (p1 == 0 ) ? p2 + 1 : i + 3
+        elseif opcode == 7
+            input[input[i + 3] + 1] = (p1 < p2) ? 1 : 0
+            global i = i + 4
+        elseif opcode == 8
+            input[input[i + 3] + 1] = (p1 == p2) ? 1 : 0
+            global i = i + 4
+        end
+    end
+end
+
+RunIntcode(copy(inputData), 1)
+RunIntcode(copy(inputData), 5)
