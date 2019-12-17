3ea1362acb948f602ee6aaba7ae1f5d5dd9d96eb
csLinhart <36882208+csLinhart@users.noreply.github.com>
2019-12-17 11:55:27 +0100

    Add day 13

---
+++ D13.jl
@@ -0,0 +1,57 @@
+using DelimitedFiles
+inputData = readdlm("D13.txt", ',', Int)
+
+function RunIntcode(input, initInp)
+    i = 1
+    rb = 0
+    p = Int.(zeros(1, 3))
+    result = []
+    while true
+        code = parse.(Int, split(lpad(string(input[i]), 5, '0'), ""))
+        opcode = code[end - 1]  * 10 + code[end]
+        for k = 1:3
+            if code[4 - k] == 0
+                p[k] = input[i + k] + 1
+            elseif code[4 - k] == 1
+                p[k] = i + k
+            elseif code[4 - k] == 2
+                p[k] = input[i + k] + 1 + rb
+            end
+        end
+        if opcode == 1
+            input[p[3]] = input[p[1]] + input[p[2]]
+            i = i + 4
+        elseif opcode == 2
+            input[p[3]] = input[p[1]] * input[p[2]]
+            i = i + 4
+        elseif opcode == 3
+            input[p[1]] = initInp
+            i = i + 2
+        elseif opcode == 4
+            push!(result, input[p[1]])
+            i = i + 2
+        elseif opcode == 5
+            i = (input[p[1]] != 0 ) ? input[p[2]] + 1 : i + 3
+        elseif opcode == 6
+            i = (input[p[1]] == 0 ) ? input[p[2]] + 1 : i + 3
+        elseif opcode == 7
+            input[p[3]] = (input[p[1]] < input[p[2]]) ? 1 : 0
+            i = i + 4
+        elseif opcode == 8
+            input[p[3]] = (input[p[1]] == input[p[2]]) ? 1 : 0
+            i = i + 4
+        elseif opcode == 9
+            rb = rb + input[p[1]]
+            i = i + 2
+        elseif opcode == 99
+            return result
+        end
+    end
+end
+
+largeMemory = Int.(zeros(1, 5000))
+largeMemory[1:length(inputData)] = inputData
+output = RunIntcode(copy(largeMemory), 0)
+output = reshape(output, 3, :)'
+tileIds = output[:,3]
+println(length(tileIds[tileIds .== 2]))
