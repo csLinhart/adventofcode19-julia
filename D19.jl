2f3b72005627f64fe939d12de77abfc38ca5aea9
csLinhart <36882208+csLinhart@users.noreply.github.com>
2019-12-21 12:11:51 +0100

    Add day 19

--- D19.jl
+++ D19.jl
@@ -3,12 +3,10 @@ include("IntCode.jl")
 inputData = readdlm("D19.txt", ',', Int)
 
 function CalculatePart1(inputData)
-    largeMemory = zeros(Int, 1, 500)
-    largeMemory[1:length(inputData)] = inputData
     count = 0
     for k = 0:49
         for l = 0:49
-            result = RunIntcode(inputData, [k, l], 500, true)
+            result = RunIntcode(inputData, [k, l], true)
             count += result
         end
     end
@@ -17,13 +15,11 @@ end
 println(CalculatePart1(inputData))
 
 function CalculatePart2(inputData)
-    largeMemory = zeros(Int, 1, 500)
-    largeMemory[1:length(inputData)] = inputData
     for k = 800:1400
         for l = 800:1400
-            if RunIntcode(inputData, [k, l], 500, true) == 1
-                if RunIntcode(inputData, [k + 99, l], 500, true) == 1
-                    if RunIntcode(inputData, [k, l + 99], 500, true) == 1
+            if RunIntcode(inputData, [k, l], true) == 1
+                if RunIntcode(inputData, [k + 99, l], true) == 1
+                    if RunIntcode(inputData, [k, l + 99], true) == 1
                         return k * 10000 + l
                     end
                 end
