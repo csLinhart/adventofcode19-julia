4539be1d8c77574310675a6fbb0db0a30eb81d18
csLinhart <36882208+csLinhart@users.noreply.github.com>
2019-12-21 12:14:18 +0100

    Add day 21

--- D21.jl
+++ D21.jl
@@ -2,10 +2,8 @@ using DelimitedFiles
 include("IntCode.jl")
 inputData = readdlm("D21.txt", ',', Int)
 
-largeMemory = zeros(Int, 1, 2500)
-largeMemory[1:length(inputData)] = inputData
 instruction1 = "NOT A T\nNOT B J\nOR T J\nNOT C T\nOR T J\nAND D J\nWALK\n"
-println(RunIntcode(copy(largeMemory), Int.(Vector{Char}(instruction1)))[end])
+println(RunIntcode(inputData, Int.(Vector{Char}(instruction1)))[end])
 
 instruction2 = "NOT A T\nNOT B J\nOR T J\nNOT C T\nOR T J\nAND D J\nNOT D T\nOR H T\nOR E T\nAND T J\nRUN\n"
-println(RunIntcode(copy(largeMemory), Int.(Vector{Char}(instruction2)))[end])
+println(RunIntcode(inputData, Int.(Vector{Char}(instruction2)))[end])
