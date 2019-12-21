0b71c91216d0380a6231dc6e163c78f4238ae386
csLinhart <36882208+csLinhart@users.noreply.github.com>
2019-12-21 12:03:21 +0100

    Add intcode

--- D19.jl
+++ D19.jl
@@ -1,10 +1,10 @@
-using DelimitedFiles
-inputData = readdlm("D19.txt", ',', Int)
-
-function RunIntcode(input, initInp)
+function RunIntcode(inputData, initInp, size, earlyExit = false)
+    input = zeros(Int, 1, size)
+    input[1:length(inputData)] = inputData
     i = 1
     rb = 0
     p = zeros(Int, 1, 3)
+    output = []
     while true
         code = parse.(Int, split(lpad(string(input[i]), 5, '0'), ""))
         opcode = code[end - 1]  * 10 + code[end]
@@ -27,7 +27,10 @@ function RunIntcode(input, initInp)
             input[p[1]] = popfirst!(initInp)
             i = i + 2
         elseif opcode == 4
-            return input[p[1]]
+            if earlyExit
+                return input[p[1]]
+            end
+            push!(output, input[p[1]])
             i = i + 2
         elseif opcode == 5
             i = (input[p[1]] != 0 ) ? input[p[2]] + 1 : i + 3
@@ -43,38 +46,7 @@ function RunIntcode(input, initInp)
             rb = rb + input[p[1]]
             i = i + 2
         elseif opcode == 99
-            return
+            return output
         end
     end
 end
-
-function CalculatePart1(inputData)
-    largeMemory = zeros(Int, 1, 500)
-    largeMemory[1:length(inputData)] = inputData
-    count = 0
-    for k = 0:49
-        for l = 0:49
-            result = RunIntcode(copy(largeMemory), [k, l])
-            count += result
-        end
-    end
-    count
-end
-println(CalculatePart1(inputData))
-
-function CalculatePart2(inputData)
-    largeMemory = zeros(Int, 1, 500)
-    largeMemory[1:length(inputData)] = inputData
-    for k = 800:1500
-        for l = 800:1500
-            if RunIntcode(copy(largeMemory), [k, l]) == 1
-                if RunIntcode(copy(largeMemory), [k + 99, l]) == 1
-                    if RunIntcode(copy(largeMemory), [k, l + 99]) == 1
-                        return k * 10000 + l
-                    end
-                end
-            end
-        end
-    end
-end
-println(CalculatePart2(inputData))
