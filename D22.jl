981da3769403c5fdd3fc834459ab850f1f25eb0c
csLinhart <36882208+csLinhart@users.noreply.github.com>
2019-12-22 12:49:03 +0100

    Add day 22 part 1

---
+++ D22.jl
@@ -0,0 +1,30 @@
+using DelimitedFiles
+inputData = readdlm("D22.txt", '\n', String)
+inputData = split.(inputData, " ")
+
+function ShuffleCards(inputData)
+    cards = collect(0:10006)
+    function Shuffle(cards, inst)
+        if inst[1] == "cut"
+            cut = parse(Int, inst[2])
+            cards = cut > 0 ? append!(cards[cut + 1:end], cards[1:cut]) : prepend!(cards[1:length(cards) + cut], cards[length(cards) + cut + 1:length(cards)]) 
+        elseif inst[2] == "into"
+            cards = reverse(cards)
+        elseif inst[2] == "with"
+            inc = parse(Int, inst[4])
+            ind = 1
+            cop = copy(cards)
+            for j = 1:length(cop)
+                cards[ind] = cop[j]
+                ind += inc
+                if ind > length(cards)
+                    ind = ind - length(cards)
+                end
+            end
+        end
+        cards
+    end
+    cards = reduce(Shuffle, inputData; init = cards)
+    findfirst(isequal(2019), cards) - 1
+end
+println(ShuffleCards(inputData))
